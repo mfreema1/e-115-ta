ee399b287e1ec6114e6d8a3c8b9d445b630d48ac
mfreema1 <mfreema1@stevens.edu>
2018-09-20 22:05:44 -0400

    Add section on typecasting

--- lab3/numbers.cpp
+++ lab3/numbers.cpp
@@ -1,8 +1,7 @@
 #include<iostream> //define cout and cin
 #include<bits/stdc++.h> //define int max and min
 #include<float.h> //define float and double max and min
-#include <typeinfo> //let us see types
-using namespace std;
+using namespace std; //we're using the standard library
 
 /**
  * Things to go over in class:
@@ -11,11 +10,12 @@ using namespace std;
  * 3. Floats and doubles - how are they different?
  * 4. How do cin and cout work?
  * Reference: http://www.cplusplus.com/doc/tutorial/variables/
+ * 
+ * NOTE: To view how certain parts of this code work, you can
+ * comment out sections by highlighting them with your mouse
+ * and hitting "ctrl + /".  You can also add a "//" in front
+ * of any line to comment out that line.
  * */
-
-// 
-
-
 
 int main() {
 
@@ -26,8 +26,7 @@ int main() {
     int x;
     x = 5;
 
-    int y = 10;
-    cout << typeid(y).name() << endl;
+    int y = 10; //can use either form, each useful in own way
 
     //how big is an int?
     cout << "Biggest size of int: " << INT_MAX << endl;
@@ -40,14 +39,12 @@ int main() {
      * to represent data and perform operations.  Every
      * binary digit - 0 or 1 - is called a bit.  An int
      * is 32 bits.
-     * 
      * */
 
     //====================
 
     //what is a float?
     float f = 5.0;
-    cout << typeid(f).name() << endl;
 
     /**
      * Float stands for floating point.  This is
@@ -66,7 +63,6 @@ int main() {
 
     //and a double?
     double d = 5.0;
-    cout << typeid(d).name() << endl;
 
     /**
      * Double is also a floating point type.  It is called
@@ -91,7 +87,7 @@ int main() {
      * */
     int elf;
     cout << "Buddy the elf here, what's your favorite number?" << endl;
-    cin >> elf; //stop and wait for input from user
+    cin >> elf; //stop and wait for input from user, store in elf
     cout << "Yeah, I like " << elf << " too!" << endl;
 
     /**
@@ -126,4 +122,47 @@ int main() {
 
     //When you combine operands and operators, you will get what is
     //called an expression, which we'll save for another time.
+
+    //MINI-MODULE: Typecasting
+    //==================== 
+
+    //It's worth noting that operators such as division (/) look at the
+    //types of its operands.
+
+    //Take for example:
+    cout << "Integers 5/4: " << 5 / 4 << endl; //result is 1 because both are ints
+
+    //This is a weird behavior at first.  When both operands are ints,
+    //division results in an int.  Division of two ints does the division
+    //and the decimal portion is "thrown out" or always rounded down.
+
+    //Precision:
+    //Double is the highest precision because it has the most bits, followed
+    //by float and then int at the lowest precision.  The result of a division
+    //is always the data type of the highest precision operand.
+
+    //For example: 
+        //If we divide a float and an int, the result is a float because
+        //float is higher precision.
+
+        //If we divide a double and an int, the result is a double because the double
+        //is the higher precision.
+
+        //If we divide a double and a float, the result is a double because the double
+        //is higher precision than a float.
+
+    //To get our ints to act like a float or a double, we can perform a typecast.
+    //A typecast essentially tells one data type to act like another in an expression.
+    
+    //A typecast is denoted by the form:
+        // (type_to_cast_to)value
+
+    //So, to get the above equation to return a float, we could do:
+    cout << "Floated 5/4: " << (float)5 / 4 << endl; //result is a float
+
+    //Or we could do something like:
+    cout << "Double 5/4: " << (double)5 / 4 << endl; //result is a double
+
+    //Typecasting works with all data types, beyond just these examples, and is
+    //a very useful tool to master.
 }
